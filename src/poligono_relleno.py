3d699ba84830ea4c40e9229fcd4aa262cc81cdb0
Jose Angel Garcia <chepeaicrag12@gmail.com>
2021-05-20 00:48:49 -0500

    Mejorando el algoritmo de rellnado de poligono

--- src/poligono_relleno.py
+++ src/poligono_relleno.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from Libreria.Functions import line, traslacion, rotacion, escalado
-from Libreria.Utils import combinacion
 import matplotlib
 from math import  pi, cos, sin
 
@@ -20,73 +19,69 @@ def imprimir_linea():
   plt.colorbar()
   plt.show()
 
-def draw_point(x, y, c):
-  linea.append([y, x])
+def draw_point(p = (0, 0)):
+  x, y = p
+  if x > 0 and y > 0: 
+    linea.append([y, x])
 
-def paint_lines(points, color):
-  for i in points:
-      for j in line(i[0], i[1]):
-          x, y = j
-          draw_point(x, y, color)
+"""
+  Función que dada una lista de tuplas, donde cada tupla es un punto, entonces se pinta el pixel con respectiva
+  coordenada
+"""
+def paint_lines(points):
+  for i in points: # [(), (), (), ()]
+    draw_point(i)
 
-def get_vertices(puntos):
-  l = len(puntos) - 1
-  points = []
-  for i in range(l):
-    points.append([puntos[i], puntos[i + 1]])
-  points.append([puntos[0], puntos[-1]])
-  return points
+# [[(x0, y0), (x1, y1)]]
+"""
+  Función que dada una lista de listas que contienen cada una el punto inicial y final se genera cada una de 
+  las lineas, y sus respectivos puntos de todas las lineas formadas se almacenan en una lista 
+  points = [[(x0, y0), (x1, y1)], [(xn-1, yn-1), (xn, yn)]]
+  return [(x0, y0) ... (x1, y1), (xn-1, y0) ... (xn, yn)] # Lista de tuplas
+"""
+def get_points_lines(points):
+  return [ j for i in points for j in line(i[0], i[1]) ] 
+  
+""" 
+  Es una función que dada una lista de puntos [(x, y), (x1, y1) ... (xn, yn)] retorna
+  una lista de listas que tienen el punto inicial y final de la linea que forma el contorno 
+  [[(x, y), (x1, y1)], [(x1, y1), (x2, y2)] ... [(xn-1, yn-1), (xn, yn)], [(xn, yn), (x, y)]]
+"""
+def get_vertices(points):
+  l = len(points)
+  return [ [ points[i], points[i + 1 if i < l - 1 else 0] ] for i in range(l) ]
 
-def rellenar(vertices, xmax, ymax):
-  vertices_act = []
-  for i in vertices:
-    points = line(i[0], i[1])
-    for p in points:
-        vertices_act.append(p)
-  l = len(vertices_act)
-  print(vertices_act)
-  pixels = {}
+"""
+  Rellena a partir de una lista de tuplas (puntos) y un valor de y máximo para iterar de forma decremental
+  Trazando de izquierda a derecha de forma horizontal los pixeles de adentro del contorno
+"""
+def rellenar(points_outline, ymax):
   values_x = []
+  print('puntos_contorno', points_outline)
   for vy in range(ymax):
-    for p  in vertices_act:
-        x, y = p
-        if y == vy:
-            values_x.append(x)
-    if len(values_x) > 0:
-        xma, xmi = max(values_x), min(values_x) 
-        pixels[vy] = [xmi, xma]
+    values_x = [ x for (x, y) in points_outline if y == vy ]
+    if len(values_x) == 0: continue
+    for xp in range(min(values_x), max(values_x)):
+      draw_point((xp, vy))
     values_x = []
-  for y, x in pixels.items():
-      for j in line((x[0], y), (x[1], y)):
-          x, y = j
-          draw_point(x, y, 'red')
-
-def maximo(points):
-    xmax, ymax = 0, 0
-    for p in points:
-        x, y = p
-        xmax = x if x > xmax else xmax
-        ymax = y if y > ymax else ymax
-    return xmax, ymax
 
 def generar_poligono(n = 0):
   if n < 3: return
   teta, t_aux = (360 / n) * pi / 180, (45) * pi / 180
-  puntos, dx, dy, d = [], 10, 10, 5 # Desplazamiento en x , y   
-  for i in range(n):
+  puntos, dx, dy, d = [], 15, 15, 10 # Desplazamiento en x , y   
+  for _ in range(n):
       x, y = round(d * cos(t_aux) + dx), round(d * sin(t_aux) + dy)
       t_aux += teta
-      print(x, y)
       puntos.append((x, y))
-  points_direction = get_vertices(puntos)
-  print(puntos)
-  xmax, ymax = maximo(puntos)
-  print (points_direction)
-  paint_lines(points_direction, 'blue')
-  rellenar(points_direction, xmax, ymax)
+  print(puntos) # Puntos de los vertices
+  points_direction = get_vertices(puntos) # La combinación entre esos vertices para marcar los puntos de las líneas
+  ymax = max( [ y for x, y in puntos ] ) # La y maxima para hacer el pintado de forma horizontal
+  print ('points_direction', points_direction)  
+  points_lines = get_points_lines(points_direction) # Lista con todas los puntos de todas las lineas para el contorno
+  paint_lines(points_lines) # Pinta las lineas de alrededor del poligono 
+  rellenar(points_lines, ymax) # Pinta los pixeles interiores del poligono (relleno)
   imprimir_linea()
 
 
 if '__main__' == '__main__':
-  generar_poligono(5)
-  
+  generar_poligono(6)
